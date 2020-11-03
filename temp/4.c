c4f5e1816c4790a9d58003fcc51e100567681c9f
qinlijie <995199161@qq.com>
2020-11-02 19:49:42 -0800

    [add 4.c]

---
+++ temp/4.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(int argc, const char *argv[])
+{
+	puts("this is 4.c");
+	return 0;
+}
---
+++ temp/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(int argc, const char *argv[])
+{
+	puts("lala 3.c");
+	return 0;
+}
